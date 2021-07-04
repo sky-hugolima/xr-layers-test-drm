4400ab55180a3e6f5accf0d2b96ca843c88013d2
Hugo Lima <hugo.lima@sky.uk>
2021-07-04 18:58:54 +0100

    transform changed

--- index.js
+++ index.js
@@ -181,10 +181,10 @@ async function startExperience(videoEl, xrSession) {
         transform: new XRRigidTransform({
             x: 0.0,
             y: 1.3,
-            z: -2.75,
+            z: -3.0,
             w: 1.0,
         }),
-        width: 2,
+        width: 1.5,
     });
     xrSession.updateRenderState({
         layers: [layer],
